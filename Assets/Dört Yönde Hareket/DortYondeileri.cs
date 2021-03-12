d4c35ccfdd13445ed3c3687ae767b5a06f708791
Murad Can MANAV <56815816+MuradCan@users.noreply.github.com>
2021-03-12 15:14:59 +0300

    Add files via upload

---
+++ Assets/Dört Yönde Hareket/DortYondeileri.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DortYondeileri : MonoBehaviour
+{
+    
+    private CharacterController controller;
+    public Vector3 playerVelocity; //tek hönde hareket etmesini istiyorsanız, istediğiniz yönün velocity değerini değiştirin.(x,y,z)
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>(); 
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;         
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
